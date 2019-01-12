958cbf858fe2e0023c77d7ad13000ba13c923db8
Carlos Aguilera <a.carlosing@gmail.com>
2019-01-12 16:20:07 -0600

    Practice.

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
+// app.use((req, res, next) => {
+//     console.log('First middleware!');
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log('Second middleware!');
+//     res.send('<h1>Assignment solved (almost!)</h1>');
+// });
+
+app.use('/users', (req, res, next) => {
+    console.log('/users middleware');
+    res.send('<h1>The middleware that handles just /users</h1>');
 });
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another the middleware!');
-    res.send('<h1>The "Add Product" Page!</h1>');
-});
-
 app.use('/', (req, res, next) => {
-    console.log('In another the middleware!');
-    res.send('<h1>Hello form express!</h1>');
+    console.log('/ middleware');
+    res.send('<h1>The middleware that handles just /</h1>');
 });
 
 app.listen(3000);
