113f5a3cdab1e2501557fe45f2d42d988f200cb9
Carlos Aguilera <a.carlosing@gmail.com>
2019-01-13 10:56:31 -0600

    Converting HTML files to Pug.

--- routes/admin.js
+++ routes/admin.js
@@ -2,15 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', { docTitle: 'Add Product' });
 });
 
 // /admin/add-product => POST
