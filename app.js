9c8a0a5736a48c43de27f80ac4dbf114a41e9389
Carlos Aguilera <a.carlosing@gmail.com>
2019-01-26 22:42:57 -0600

    Fetching products.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorsController = require('./controllers/errorsController');
-const db = require('./util/database');
 
 const app = express();
 
@@ -13,14 +12,6 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-db.execute('SELECT * FROM products')
-    .then(result => {
-        console.log(result);
-    })
-    .catch(err => {
-        console.log(err)
-    });
 
 app.use(bodyParser.urlencoded({
     extended: false
