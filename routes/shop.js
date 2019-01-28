9d4b6dc8887f443c4ded6f5edfecf01fcc0b0cb6
Carlos Aguilera <a.carlosing@gmail.com>
2019-01-27 19:27:31 -0600

    Storing Carditems as Orderitems.

--- routes/shop.js
+++ routes/shop.js
@@ -16,6 +16,8 @@ router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
+router.get('/create-order', shopController.postOrder);
+
 router.get('/orders', shopController.getOrders);
 
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
