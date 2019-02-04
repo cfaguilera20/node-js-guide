62a7cb2d3009a3d988dfa9fbd63f2126d6f2ebb5
Carlos Aguilera <a.carlosing@gmail.com>
2019-02-04 16:14:43 -0600

    Adding the add to cart functionality.

--- models/user.js
+++ models/user.js
@@ -41,7 +41,7 @@ class User {
         // });
         const updatedCart = {
             items: [{
-                ...product,
+                productId: new ObjectId(product._id),
                 quantity: 1
             }]
         };
