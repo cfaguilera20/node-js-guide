e241d4163d39a84c55ef5ce76b1766c138510cc5
Carlos Aguilera <a.carlosing@gmail.com>
2019-02-06 21:53:17 -0600

    Deleting cart items.

--- routes/shop.js
+++ routes/shop.js
@@ -20,7 +20,7 @@ router.post('/cart', shopController.postCart);
 
 // router.get('/orders', shopController.getOrders);
 
-// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 // router.get('/checkout', shopController.getCheckout);
 
