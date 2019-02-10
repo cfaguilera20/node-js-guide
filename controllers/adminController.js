f21271c5f09a0ab2bf5d610db04ea92df2704a43
Carlos Aguilera <a.carlosing@gmail.com>
2019-02-10 17:28:37 -0600

    Updating products.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -48,7 +48,8 @@ exports.getEditProduct = (req, res, next) => {
                 product: product
 
             });
-        }).catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -58,15 +59,15 @@ exports.postEditProduct = (req, res, next) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
 
-    const product = new Product(
-        updatedTitle,
-        updatedPrice,
-        updatedDesc,
-        updatedImageUrl,
-        prodId
-    );
-    product
-        .save()
+    Product
+        .findById(prodId)
+        .then(product => {
+            product.title = updatedTitle
+            product.price = updatedPrice
+            product.imageUrl = updatedImageUrl
+            product.description = updatedDesc
+            return product.save();
+        })
         .then(result => {
             console.log('Updated Product!')
             res.redirect('/admin/products');
