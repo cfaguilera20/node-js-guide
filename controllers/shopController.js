73e6b1d62ba4945f091cb448f040fbb8bafc9b1e
Carlos Aguilera <a.carlosing@gmail.com>
2019-02-10 17:22:26 -0600

    Fetching a single product.

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -9,18 +9,22 @@ exports.getProducts = (req, res, next) => {
                 pageTitle: 'All Products',
                 path: '/products',
             });
-        }).catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
-    Product.findById(prodId).then(product => {
-        res.render('shop/product-detail', {
-            product: product,
-            pageTitle: product.title,
-            path: '/products',
-        });
-    }).catch(err => console.log(err));
+    Product
+        .findById(prodId)
+        .then(product => {
+            res.render('shop/product-detail', {
+                product: product,
+                pageTitle: product.title,
+                path: '/products',
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
