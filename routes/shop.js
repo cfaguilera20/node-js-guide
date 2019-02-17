f89edb8e783e29985d4ae9dee81ee8b7e9019e57
Carlos Aguilera <a.carlosing@gmail.com>
2019-02-16 22:20:36 -0600

    Getting & Displaying orders.

--- routes/shop.js
+++ routes/shop.js
@@ -18,7 +18,7 @@ router.post('/cart', shopController.postCart);
 
 router.get('/create-order', shopController.postOrder);
 
-// router.get('/orders', shopController.getOrders);
+router.get('/orders', shopController.getOrders);
 
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
