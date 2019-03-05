d3b423644aa5e956ee8ee277bd8eb501a9cb1738
Carlos Aguilera <a.carlosing@gmail.com>
2019-03-05 08:40:29 -0600

    Making Add to Cart Work Again

--- app.js
+++ app.js
@@ -39,7 +39,18 @@ app.use(
     })
 );
 
-// app.use((req, res, next) => {});
+app.use((req, res, next) => {
+    if (!req.session.user) {
+        return next();
+    }
+
+    User.findById(req.session.user._id)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
