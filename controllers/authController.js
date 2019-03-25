188910ee325fea1e59010d7dc77ec52c05feb813
Carlos Aguilera <a.carlosing@gmail.com>
2019-03-25 08:06:11 -0600

    Additional Flash Messages.

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,9 +48,16 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render('auth/signup', {
         path: '/signup',
-        pageTitle: 'Signup'
+        pageTitle: 'Signup',
+        errorMessage: message
     });
 };
 
@@ -61,6 +68,10 @@ exports.postSignup = (req, res, next) => {
     User.findOne({ email: email })
         .then(userDoc => {
             if (userDoc) {
+                req.flash(
+                    'error',
+                    'E-Mail exists already, please pick a different one.'
+                );
                 return res.redirect('/signup');
             }
             return bcrypt.hash(password, 12).then(hashedPassword => {
