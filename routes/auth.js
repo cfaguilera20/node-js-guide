ca53d5ce20c008cdb7a8581e554da72d0064c645
Carlos Aguilera <a.carlosing@gmail.com>
2019-03-29 07:30:56 -0600

    Creating the rest password form.

--- routes/auth.js
+++ routes/auth.js
@@ -19,4 +19,6 @@ router.get('/reset', authController.getReset);
 
 router.post('/reset', authController.postReset);
 
+router.post('/new-password/:token', authController.getNewPassword);
+
 module.exports = router;
